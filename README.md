8eccddb2eebe4bbf9259144cd2a825129f54a22c
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-09 20:41:48 -0400

    listar y crear clientes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Platzi Ventas
+
+## Commands
+
+```sh
+virtualenv --python=python3 venv
+pip install --editable .
+source venv/bin/activate
+```
--- clients/commands.py
+++ clients/commands.py
@@ -1,61 +1,55 @@
 import click
-
 from clients.services import ClientService
 from clients.models import Client
 
 @click.group()
-def  clients():
-	"""Manages the clients lifecycle"""
-	pass
-
+def clients():
+    """Manages the clients lifecycle"""
+    pass
 
 @clients.command()
-@click.option('-n', '--name',
-              type=str,
-              prompt=True,
-              help='The client\'s name')
-@click.option('-c', '--company',
-              type=str,
-              prompt=True,
-              help='The client\'s name')
-@click.option('-e', '--email',
-              type=str,
-              prompt=True,
-              help='The client\'s name')
-@click.option('-p', '--position',
-              type=str,
-              prompt=True,
-              help='The client\'s name')
+@click.option('-n', '--name', type=str, prompt=True, help="The client's name")
+@click.option('-c', '--company', type=str, prompt=True, help="The client's company")
+@click.option('-e', '--email', type=str, prompt=True, help="The client's email")
+@click.option('-p', '--position', type=str, prompt=True, help="The client's position")
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Creates a new client"""
     client_service = ClientService(ctx.obj['clients_table'])
     client = Client(name, company, email, position)
-
     client_service.create_client(client)
-
-
 
 @clients.command()
 @click.pass_context
 def list(ctx):
-	"""List all clients"""
-	pass
+    """List all clients"""
+    client_service = ClientService(ctx.obj['clients_table'])
+    clients_list = client_service.list_clients()
 
+    click.echo(' ID | NAME | COMPANY | EMAIL |')
+    click.echo('*' * 100)
+
+    for client in clients_list:
+        click.echo(
+            "{uid} | {name} | {company} | {email} | {position}".format(
+                uid=client['uid'],
+                name=client['name'],
+                company=client['company'],
+                email=client['email'],
+                position=client['position']
+            )
+        )
 
 @clients.command()
 @click.pass_context
-def updated(ctx, client_uid):
-	"""Updates a client"""
-	pass
-
+def update(ctx, client_uid):
+    """Updates a client"""
+    pass
 
 @clients.command()
 @click.pass_context
 def delete(ctx, client_uid):
-	"""Deletes a client"""
-	pass
-
+    """Deletes a client"""
+    pass
 
 all = clients
-
